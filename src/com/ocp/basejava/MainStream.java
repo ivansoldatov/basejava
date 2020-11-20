86f33d0e4c1574c5226f9b3fe6036f73a03d2fbd
Ivan Soldatov <ias115@yandex.ru>
2020-11-20 21:54:14 +0300

    L12 HW12 minValue oddOrEven (not one stream)

---
+++ src/com/ocp/basejava/MainStream.java
@@ -0,0 +1,28 @@
+package com.ocp.basejava;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class MainStream {
+
+    static int[] values = {9, 3, 5, 8, 3, 2, 5, 6, 2};
+
+    public static void main(String[] args) {
+        System.out.println(minValue(values));
+        System.out.println(oddOrEven(IntStream.of(values).boxed().collect(Collectors.toList())));
+    }
+
+    private static int minValue(int[] values) {
+        return Arrays.stream(values).distinct().sorted().reduce((acc, x) -> acc * 10 + x).getAsInt();
+    }
+
+    private static List<Integer> oddOrEven(List<Integer> integers) {
+        if ((integers.stream().mapToInt(x -> x).sum()) % 2 == 0) {
+            return integers.stream().filter(x -> x % 2 != 0).collect(Collectors.toList());
+        } else {
+            return integers.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
+        }
+    }
+}

71004d95c4625db649fd236fd91890f3a4a38c73
Ivan Soldatov <ias115@yandex.ru>
2020-12-08 23:31:43 +0300

    L13 working commit

---
+++ src/com/ocp/basejava/Config.java
@@ -0,0 +1,32 @@
+package com.ocp.basejava;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Config {
+    private static final File PROPS = new File("./config/resumes.properties");
+    private static final Config INSTANCE = new Config();
+    private Properties props = new Properties();
+
+    private File storageDir;
+
+    private Config() {
+        try (InputStream is = new FileInputStream(PROPS)) {
+            props.load(is);
+            storageDir = new File(props.getProperty("storage.dir"));
+        } catch (IOException e) {
+            throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
+        }
+    }
+
+    public static Config getInstance() {
+        return INSTANCE;
+    }
+
+    public File getStorageDir() {
+        return storageDir;
+    }
+}
--- src/com/ocp/basejava/MainStream.java
+++ src/com/ocp/basejava/MainStream.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class MainStream {
 
@@ -15,14 +16,16 @@ public class MainStream {
     }
 
     private static int minValue(int[] values) {
-        return Arrays.stream(values).distinct().sorted().reduce((acc, x) -> acc * 10 + x).getAsInt();
+        return Arrays.stream(values)
+                .distinct()
+                .sorted()
+                .reduce(0, (acc, x) -> acc * 10 + x);
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
+        Stream<Integer> stream = integers.stream();
         if ((integers.stream().mapToInt(x -> x).sum()) % 2 == 0) {
-            return integers.stream().filter(x -> x % 2 != 0).collect(Collectors.toList());
-        } else {
-            return integers.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
-        }
+            return stream.filter(x -> x % 2 != 0).collect(Collectors.toList());
+        } else return stream.filter(x -> x % 2 == 0).collect(Collectors.toList());
     }
 }
